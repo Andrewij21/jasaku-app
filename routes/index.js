17c6d65db8441bacfc737134e9d00219453a923d
Andrewij21 <andrewijaya988@gmail.com>
2022-08-04 15:34:08 +0700

    add get to index route

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,7 @@ require("dotenv").config();
 const route = require("express").Router();
 const userRoute = require("./authRouter");
 route.use("/auth", userRoute);
-
+route.get("/", (req, res) => {
+  res.send("helloworld");
+});
 module.exports = route;
